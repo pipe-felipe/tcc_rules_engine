0be423a4f72709127da6e8dcc3be9725a59dfe1f
pipe-felipe <alves.felipepipe@gmail.com>
2022-08-18 19:23:03 -0300

    WIP

--- cmd/controller/engine_controller.go
+++ cmd/controller/engine_controller.go
@@ -2,7 +2,7 @@ package controller
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/pipe-felipe/tcc_rules_engine/handlers"
+	"github.com/pipe-felipe/tcc_rules_engine/cmd/handlers"
 )
 
 func TransactionDataRetriever(e *echo.Echo) {
